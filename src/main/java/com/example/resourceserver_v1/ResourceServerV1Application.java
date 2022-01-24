2cebae5336d25ba91bb8c45dd9e30486ba25a74f
son <sonjm3518@dsm.hs.kr>
2022-01-24 18:30:12 +0900

    ⚙ :: init

---
+++ src/main/java/com/example/resourceserver_v1/ResourceServerV1Application.java
@@ -0,0 +1,13 @@
+package com.example.resourceserver_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resourceserver_v1/ResourceServerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourceserver_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
