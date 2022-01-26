bbea20a441bfcce11dcbcec25522847320d8640a
son <sonjm3518@dsm.hs.kr>
2022-01-26 22:06:06 +0900

    📄 :: 에러 필터

---
+++ ResourceServer/src/main/java/com/example/resourceserver/global/error/exception/ExceptionFilter.java
@@ -0,0 +1,25 @@
+package com.example.resourceserver.global.error.exception;
+
+import com.example.resourceserver.global.error.ErrorResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ExceptionFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try{
+            filterChain.doFilter(request, response);
+        }catch (BJException e){
+            ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getStatus(), e.getErrorCode().getMessage());
+            response.setStatus(e.getErrorCode().getStatus());
+            response.setContentType("application/json");
+            response.getWriter().write(errorResponse.convertToJson(errorResponse));
+        }
+    }
+}
