5127ec6b2a8a3ae128be196d505d7a59649b3176
son <sonjm3518@dsm.hs.kr>
2022-01-28 19:16:03 +0900

    ⚡ :: 에러 추가

--- AuthorizationServer/src/main/java/com/example/authorizationserver/global/error/ErrorCode.java
+++ AuthorizationServer/src/main/java/com/example/authorizationserver/global/error/ErrorCode.java
@@ -6,7 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND(404, "User Not Found");
+    USER_NOT_FOUND(404, "User Not Found"),
+
+    EXPIRED_TOKEN(401, "Expired Token"),
+    INVALID_TOKEN(401, "Invalid Token");
 
     private final int status;
     private final String message;
