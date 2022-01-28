6b6d2b0551a18d945bf07d38311f155da3fb223d
son <sonjm3518@dsm.hs.kr>
2022-01-28 19:15:11 +0900

    📄 :: 토큰 Response

---
+++ AuthorizationServer/src/main/java/com/example/authorizationserver/global/security/jwt/dto/response/TokenResponse.java
@@ -0,0 +1,11 @@
+package com.example.authorizationserver.global.security.jwt.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private final String accessToken;
+    private final String refreshToken;
+}
