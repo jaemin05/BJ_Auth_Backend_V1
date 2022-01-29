9f46adae35b44cdaa29e088f1ebf8d3e8cbf3ec6
son <sonjm3518@dsm.hs.kr>
2022-01-30 01:12:17 +0900

    📄 :: UserFacade

---
+++ AuthorizationServer/src/main/java/com/example/authorizationserver/domain/user/facade/UserFacade.java
@@ -0,0 +1,33 @@
+package com.example.authorizationserver.domain.user.facade;
+
+import com.example.authorizationserver.domain.user.api.dto.request.SignupRequest;
+import com.example.authorizationserver.domain.user.entity.User;
+import com.example.authorizationserver.domain.user.exception.UserAlreadyExistsException;
+import com.example.authorizationserver.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User registerUser(SignupRequest signupRequest){
+        isAlreadyExists(signupRequest.getUsername());
+        return userRepository.save(
+                new User(
+                        signupRequest.encodePassword(
+                                passwordEncoder.encode(signupRequest.getPassword())
+                        )
+                )
+        );
+    }
+
+    public void isAlreadyExists(String username){
+        if(userRepository.findByUsername(username).isPresent()){
+            throw UserAlreadyExistsException.EXCEPTION;
+        }
+    }
+}
