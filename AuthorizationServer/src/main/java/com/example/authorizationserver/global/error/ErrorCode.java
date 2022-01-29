ccb007539574f880cc0502a5d3e3f7ff1c168a4d
son <sonjm3518@dsm.hs.kr>
2022-01-30 01:11:34 +0900

    ⚡ :: 에러 추가

--- AuthorizationServer/src/main/java/com/example/authorizationserver/global/error/ErrorCode.java
+++ AuthorizationServer/src/main/java/com/example/authorizationserver/global/error/ErrorCode.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     USER_NOT_FOUND(404, "User Not Found"),
+    USER_ALREADY_EXISTS(409, "User Already Exists"),
 
     EXPIRED_TOKEN(401, "Expired Token"),
     INVALID_TOKEN(401, "Invalid Token");
