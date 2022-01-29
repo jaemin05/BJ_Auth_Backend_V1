d3a5931126d37be7751148a6a0850408578cc02d
son <sonjm3518@dsm.hs.kr>
2022-01-30 00:39:01 +0900

    ⚡ :: @Id

--- AuthorizationServer/src/main/java/com/example/authorizationserver/domain/refreshtoken/entiry/RefreshToken.java
+++ AuthorizationServer/src/main/java/com/example/authorizationserver/domain/refreshtoken/entiry/RefreshToken.java
@@ -3,12 +3,10 @@ package com.example.authorizationserver.domain.refreshtoken.entiry;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
-
-import javax.persistence.Id;
-
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
