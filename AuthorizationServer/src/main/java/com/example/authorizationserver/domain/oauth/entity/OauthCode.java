7f68eed40a2204014d0bb1c7c61cd42ab2e7714d
son <sonjm3518@dsm.hs.kr>
2022-01-30 00:39:15 +0900

    📄 :: oauthcode

---
+++ AuthorizationServer/src/main/java/com/example/authorizationserver/domain/oauth/entity/OauthCode.java
@@ -0,0 +1,25 @@
+package com.example.authorizationserver.domain.oauth.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash
+public class OauthCode {
+    @Id
+    private String clientId;
+
+    private String code;
+
+    private Long ttl;
+
+    public OauthCode(String clientId, String code){
+        this.clientId = clientId;
+        this.code = code;
+        this.ttl = 180L;
+    }
+}
