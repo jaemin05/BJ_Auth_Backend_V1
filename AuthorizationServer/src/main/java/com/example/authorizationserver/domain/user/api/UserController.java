b40fa3dc32c3f96180810f60e7063c962228c9ec
son <sonjm3518@dsm.hs.kr>
2022-01-30 01:10:41 +0900

    📄 :: 유저 Controller 작성

--- AuthorizationServer/src/main/java/com/example/authorizationserver/domain/user/api/UserController.java
+++ AuthorizationServer/src/main/java/com/example/authorizationserver/domain/user/api/UserController.java
@@ -1,4 +1,22 @@
 package com.example.authorizationserver.domain.user.api;
 
+import com.example.authorizationserver.domain.user.api.dto.request.SignupRequest;
+import com.example.authorizationserver.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signup(@RequestPart @Valid SignupRequest signupRequest){
+        userService.signup(signupRequest);
+    }
 }
