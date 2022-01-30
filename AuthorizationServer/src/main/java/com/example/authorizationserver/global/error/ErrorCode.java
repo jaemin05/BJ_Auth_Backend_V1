e14a1074577e100c0876db489ebecf01e6b0fec2
son <sonjm3518@dsm.hs.kr>
2022-01-30 23:18:45 +0900

    ⚡ :: 에러 추가

--- AuthorizationServer/src/main/java/com/example/authorizationserver/global/error/ErrorCode.java
+++ AuthorizationServer/src/main/java/com/example/authorizationserver/global/error/ErrorCode.java
@@ -10,7 +10,11 @@ public enum ErrorCode {
     USER_ALREADY_EXISTS(409, "User Already Exists"),
 
     EXPIRED_TOKEN(401, "Expired Token"),
-    INVALID_TOKEN(401, "Invalid Token");
+    INVALID_TOKEN(401, "Invalid Token"),
+
+    CLIENT_NOT_FOUND(404, "Client Not Found"),
+    INVALID_CLIENT_SECRET(401, "Invalid Client Secret"),
+    INVALID_OAUTH_CODE(401, "Invalid Oauth Code");
 
     private final int status;
     private final String message;
